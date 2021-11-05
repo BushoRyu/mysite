d55c8f8f581dd679b9dc275eff5ac13c5a637490
BushoRyu <ryugon.s.0829@gmail.com>
2021-11-05 21:03:29 +0900

    Chat

--- .ipynb_checkpoints/Main-checkpoint.py
+++ .ipynb_checkpoints/Main-checkpoint.py
@@ -10,7 +10,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info["USER_ID"]
-    messages = TextSendMessage(text="おはよう〜！ \n　大丈夫？")
+    messages = TextSendMessage(text="早く起きろ〜！")
     line_bot_api.push_message(USER_ID, messages=messages)
     
 if __name__ == "__main__":
